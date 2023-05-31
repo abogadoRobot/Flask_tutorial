35d30a7ba0b64e5b8346370933b885baf9cd741a
abogadoRobot <85517452+abogadoRobot@users.noreply.github.com>
2023-05-31 17:18:25 +0000

    added mil weas

--- app.py
+++ app.py
@@ -1,11 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Lonqui',
+    'salary': '100 pesos'
+  },
+  {
+    'id': 2,
+    'title': 'Cercenador de cabezas',
+    'location': 'Temuco',
+    'salary': '50 pesos'
+  },
+  {
+    'id': 3,
+    'title': 'Aweonador',
+    'location': 'Banana',
+    'salary': '1 pesos'
+  },
+  {
+    'id': 4,
+    'title': 'Comedor de caca',
+    'location': 'NYC',
+    'salary': '0 pesos'
+  },
+]
 
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS)
+
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == "__main__":
